983b283c2991b2f085ba3bc94f44851adb41684d
PrasantPradeepcmd <ppradeep@conestogac.on.ca>
2025-05-05 20:09:41 -0400

    New changes in hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import memojiImage from "@/assets/images/memoji-computer.png";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg"
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48">
+    <div className="py-32 md:py-48 lg:py-60">
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
