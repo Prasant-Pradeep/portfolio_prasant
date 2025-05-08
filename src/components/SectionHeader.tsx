61eb806584e1806bdf53592ad676f57552bfd962
Prasant-Pradeep <pcprasant376@gmail.com>
2025-05-08 00:03:43 -0400

    End of day push

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,19 @@
+export const SectionHeader = () => {
+    return (
+        <>
+            <div className="flex justify-center">
+            <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+                Real-World Results
+            </p>
+            </div>
+
+            <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+            Featured Projects
+            </h2>
+            <p className="text-center md:text-lg text-white/60 mt-4 max-w-md
+            mx-auto">
+            See how I transformed concepts into engagigndigital expereiences
+            </p>
+        </>
+    );
+};
