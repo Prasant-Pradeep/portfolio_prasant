f2c7b5875730fde3c0c317a10d993bba19a929fe
Prasant-Pradeep <pcprasant376@gmail.com>
2025-05-13 11:34:17 -0400

    feat: Adding about section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -50,7 +50,7 @@ export const TestimonialsSection = () => {
           description="Don't just take my word for it. See what my clients have to say about my work."
         />
 
-        <div className="mt-16 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex gap-8 flex-none">
             {testimonials.map((testimonial) => (
               <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
