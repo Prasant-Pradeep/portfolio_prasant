7ba0719983ee29c4ec114dd64742eca77e6b20c9
Prasant-Pradeep <pcprasant376@gmail.com>
2025-05-19 14:52:24 -0400

    feat:project section animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -5,10 +5,18 @@ export const HeroOrbit = ({
   children,
   size,
   rotation,
-  shouldOrbit,
+  shouldOrbit = false,
   orbitDuration,
-}: PropsWithChildren<{ size: number; rotation: number;
-shouldOrbit?: boolean; orbitDuration?: string; }>) => {
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{ 
+  size: number; 
+  rotation: number;
+  shouldOrbit?: boolean; 
+  shouldSpin?: boolean;
+  spinDuration?: string;
+  orbitDuration?: string; 
+}>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div className={twMerge(shouldOrbit === true &&
@@ -23,9 +31,10 @@ shouldOrbit?: boolean; orbitDuration?: string; }>) => {
           width: `${size}px`,
         }}
       >
-        <div className="animate-spin [animation-duration:10s">
-
-
+        <div className={twMerge(shouldSpin === true &&
+        "animate-spin")} style={{
+          animationDuration: spinDuration
+        }}>
           <div
             className="inline-flex"
             style={{
